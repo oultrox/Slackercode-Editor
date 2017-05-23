ed29869763ffc6900a03190ac60804a6fe2126df
Slacker <x3mworks@gmail.com>
2017-05-23 10:07:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ SCE.CodeEditor.JS = {};
 (function () {
     $(document).ready(function () {
 
-        $('body').addClass('ui loader');  
+        $('body').wrap('<div class="ui inverted segment"><div class="ui active inverted loader"></div></div>');  
         $('sui-browser').load("sections/browser/browser.html", function () {
                 
         });
