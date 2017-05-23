a05725be85739cfa7457d95d09896f5942083d27
Slacker <x3mworks@gmail.com>
2017-05-23 10:29:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ SCE.CodeEditor.JS = {};
 
         $('sui-toolbar').load("sections/toolbar/toolbar.html", function () {
                 $('#loadingbar').css('width','30%');
-            alert(1);
+           
 
         });   
         
         $('sui-browser').load("sections/browser/browser.html", function () {
                 $('#loadingbar').css('width','60%');
-            alert(2);
+            
         });
         
         $('sui-codeeditor').load("sections/codeeditor/codeeditor.html", function () {
                 $('#loadingbar').css('width','100%');
-            alert(3);
+            
         });
 
 
