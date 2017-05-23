13fe399f1a60d2bdd0c6ce43f583498b5aa20ed6
Slacker <x3mworks@gmail.com>
2017-05-23 10:25:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ SCE.CodeEditor.JS = {};
 
 (function () {
     
-    $('sui-loader').html('<div id="loadingbar"></div>');
+    //$('sui-loader').prepend('<div id="loadingbar"></div>');
     $(document).ready(function () {
 
         
