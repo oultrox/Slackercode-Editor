321a5245c41fa2f45234b43305e6b3307a2eb8a7
Slacker <x3mworks@gmail.com>
2017-05-23 10:22:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ SCE.CodeEditor.JS = {};
 
 (function () {
     
-    $('sui-loader').html('<div></div>');
+    $('sui-loader').html('<div id="loadingbar"></div>');
     $(document).ready(function () {
 
         
         
         $('sui-browser').load("sections/browser/browser.html", function () {
-                
+                $('#loadingbar').css('width','30%');
         });
         
         $('sui-codeeditor').load("sections/codeeditor/codeeditor.html", function () {
-
+                $('#loadingbar').css('width','60%');
         });
         $('sui-toolbar').load("sections/toolbar/toolbar.html", function () {
+                $('#loadingbar').css('width','100%');
 
         });
 
